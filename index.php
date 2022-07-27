98e4922b72e99fe67f5a7a2ee5c3784a17f0fa54
unknown <rajnish@aspireindia.com>
2022-07-27 19:21:43 +0530

    commiting on php and css file

--- index.php
+++ index.php
@@ -4,21 +4,23 @@
   <meta charset="utf-8">
   <title>PHP Contact Form with Captcha</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="asset/css/style.css">
 </head>
 <body>
-  <div class="container mt-5">
+  <div class="row">
+  <div class="container mt-5 header">
+    <div class="col-md-12">
     <?php include('contact.php'); ?>
     <!-- Captcha error message -->
     <?php if(!empty($captchaError)) {?>
       <div class="form-group col-12 text-center">
-        <div class="alert text-center <?php echo $captchaError['status']; ?>">
+        <div class="alert text-center <?php echo $captchaError['status'];?> message">
           <?php echo $captchaError['message']; ?>
         </div>
       </div>
     <?php }?>
     <!-- Contact form -->
-    <form action="" name="contactForm" method="post" enctype="multipart/form-data">
+    <form action="" name="contactForm" method="post" enctype="multipart/form-data" class="regform">
       <div class="form-group">
         <label>Name</label>
         <input type="text" class="form-control" name="name" id="name">
@@ -33,12 +35,14 @@
           <input type="text" class="form-control" name="captcha" id="captcha">
         </div>
         <div class="form-group col-6">
-          <label>Captcha Code</label>
+          <label class="captcha">Captcha Code</label>
           <img src="captcha.php" alt="PHP Captcha">
         </div>
       </div>
       <input type="submit" name="send" value="Send" class="btn btn-dark btn-block">
     </form>
   </div>
+</div>
+</div>
 </body>
 </html>
